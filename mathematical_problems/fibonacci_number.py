c16bdd8e127bcbe390d7ecc541e6be331cf1991e
Tarik <tsahin069@gmail.com>
2020-08-16 23:19:10 +0300

    Python code is done for Fibonacci numbers

---
+++ mathematical_problems/fibonacci_number.py
@@ -0,0 +1,27 @@
+# Fibonacci number
+
+# definition: Each number is the sum of the two processinf ones starting from 0 and 1
+
+def Fibonacci(n):
+    container = []
+    if n==1:
+        container.append(0)
+        return "First"+" "+str(n)+" Fibonnacci numbers are " + str(container)
+    elif n==2:
+        container.append(0)
+        container.append(1)
+        return "First"+" "+str(n)+" Fibonnacci numbers are " + str(container)
+    else:
+        container.append(0)
+        container.append(1)
+        k = 0
+        for i in range(n-2):
+            dummy = container[k]+container[k+1]
+            container.append(dummy)
+            k += 1
+        return "First"+" "+str(n)+" Fibonnacci numbers are " + str(container)
+print(Fibonacci(10))
+
+    
+
+
